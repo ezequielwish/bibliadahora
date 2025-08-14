123f21fe1492e0e5d9defdef951a64853ab5a630
Ezequiel Almeida <ezequielalmeida.wish@gmail.com>
2025-08-14 12:04:32 -0300

    add github icon

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,11 +1,15 @@
 import styles from './footer.module.css'
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer(){
     return (
         <footer className={styles.footer}>
+          <a href="https://github.com/ezequielwish">
           <span>
-            Desenvolvido por <a href="https://github.com/ezequielwish">Ezequiel Almeida</a>
+            Desenvolvido por Ezequiel Almeida 
           </span>
+          <FaGithub/>
+          </a>
         </footer>
     )
 }
