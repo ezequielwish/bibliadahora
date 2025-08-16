e8e6a265eb38c6100e59dcb2fbddcd2d5b5f9ca8
Ezequiel Almeida <ezequielalmeida.wish@gmail.com>
2025-08-15 21:42:09 -0300

    add summary

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,15 +1,13 @@
-import styles from './footer.module.css'
+import styles from "./footer.module.css";
 import { FaGithub } from "react-icons/fa";
 
-export default function Footer(){
+export default function Footer() {
     return (
         <footer className={styles.footer}>
-          <a href="https://github.com/ezequielwish">
-          <span>
-            Desenvolvido por Ezequiel Almeida 
-          </span>
-          <FaGithub/>
-          </a>
+            <a href="https://github.com/ezequielwish">
+                <span>Desenvolvido por Ezequiel Almeida</span>
+                <FaGithub />
+            </a>
         </footer>
-    )
+    );
 }
