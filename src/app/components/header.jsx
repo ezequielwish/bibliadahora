f84699e7475f5fa0c4a409fb4aae6ae23e4ac4c4
Ezequiel Almeida <ezequielalmeida.wish@gmail.com>
2025-08-20 03:20:43 -0300

    add logo and brand style

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -17,7 +17,10 @@ export default function Header() {
 
     return (
         <header className={styles.header}>
-            <h1>Biblia da hora</h1>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src="/assets/logo.png" alt="logo" />
+                <h1>Biblia da hora</h1>
+            </div>
             <button className={styles.themeButton} onClick={toggleTheme}>
                 {theme === "light" ? <FiMoon /> : <FiSun />}
             </button>
